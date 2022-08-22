ae073c18438832f822434f5b4e6fba6f6e694707
yusuparipin30 <yusuparipin30@gmail.com>
2022-08-22 13:46:01 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".reviews-slider", {
+    grabCursor:true,
+    loop:true,
+    autoHeight:true,
+    spaceBetween: 20,
+    breakpoints: {
+       0: {
+         slidesPerView: 1,
+       },
+       700: {
+         slidesPerView: 2,
+       },
+       1000: {
+         slidesPerView: 3,
+       },
+    },
+ });
+ 
